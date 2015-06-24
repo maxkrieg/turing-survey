3b83489971d2ec18e2e58ec4eddce9e77558b344
maxkrieg <maxwell.krieg@gmail.com>
2015-06-24 16:23:58 -0400

    adds api route to get specific survey

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,6 +2,7 @@ var express = require('express');
 var apiRouter = express.Router();
 var Survey = require('../lib/surveys.js');
 //////////////////////////////////////////
+
 apiRouter.get('/', function(req, res) {
   Survey.find({}, function(err, surveyList) {
     if (err) {
@@ -9,8 +10,20 @@ apiRouter.get('/', function(req, res) {
       res.sendStatus(404);
     }
     res.json(surveyList);
-  })
-})
+  });
+});
+
+apiRouter.get('/:id', function(req, res) {
+  Survey.find({
+    _id: req.params.id
+  }, function(err, survey) {
+    if (err) {
+      console.log(err);
+      res.sendStatus(404);
+    }
+    res.json(survey);
+  });
+});
 
 
 
