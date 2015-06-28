686ff700822cd5a4c0028003ae673e40f2ca5043
maxkrieg <maxwell.krieg@gmail.com>
2015-06-28 14:06:47 -0400

    adds hiding and showing of question type options in edit question form

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -24,9 +24,63 @@ $(document).ready(function() {
   // Toggle hide and show of edit-question form
   $('.edit-question').on('click', function(e) {
     e.preventDefault();
+    $('.edit-question-form').hide();
     var questionId = $(this).attr('data-id');
     $('[data-question="' + questionId + '"]').toggle();
+    console.log($(this));
+
+    var legacyType = $(this).parents('.list-group-item').find('.edit-type-select option:selected').text();
+    console.log(legacyType);
+
+    // Hides or Shows based on legacy question type
+    var renderInputs = function(questionType) {
+      if (questionType === "Multiple Choice") {
+        $('.scale').hide();
+        $('.text').hide();
+        $('.multiple-choice').show();
+      } else if (questionType === "Scale") {
+        $('.multiple-choice').hide();
+        $('.text').hide();
+        $('.scale').show();
+      } else if (questionType === "Text") {
+        $('.multiple-choice').hide();
+        $('.scale').hide();
+        $('.text').show();
+      }
+    };
+    renderInputs(legacyType);
+
+    // Hides or shows based on newly selected question type
+    var thisSelector = $(this).parents('.list-group-item').find('.edit-type-select');
+    $(thisSelector)
+      .change(function() {
+        var selectedType = "";
+        $(this).parents('.list-group-item').find('.edit-type-select option:selected').each(function() {
+          selectedType += $(this).text();
+        });
+        console.log(selectedType);
+        renderInputs(selectedType);
+      });
+
   });
+
+  // $(".edit-type-select")
+  //   .change(function() {
+  //     var selectedType = "";
+  //     $(".edit-type-select option:selected").each(function() {
+  //       selectedType += $(this).text() + " ";
+  //     });
+  //     console.log(selectedType);
+  //     if (selectedType === "Multiple Choice") {
+  //       $('.multiple-choice').show();
+  //       $('.scale').hide();
+  //       $('.text').hide();
+  //     }
+  //   })
+  //   .change();
+
+
+
 
   // Delete a Question from Survey
   $('.delete-question').on('click', function(e) {
