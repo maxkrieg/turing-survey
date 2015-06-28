6f05b222dcd2a9e69f574d74ca244dbe8e8f8961
maxkrieg <maxwell.krieg@gmail.com>
2015-06-28 11:21:44 -0400

    adds hide/show functionality for edit question form

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,18 +1,30 @@
-// $(document).ready(function() {
-$('.delete-survey').on('click', function(e) {
-  e.preventDefault();
-  var surveyId = $(this).attr('data-id');
-  var $thisButton = $(this);
+$(document).ready(function() {
 
-  $.ajax({
-    url: '/surveys/' + surveyId,
-    type: 'DELETE'
-  }).done(function() {
-    console.log('success deleting survey');
-    $thisButton.parents('.list-group-item').remove();
-  }).fail(function() {
-    console.log("error DELETING SURVEY");
+  $('.edit-question-form').hide();
+
+  $('.delete-survey').on('click', function(e) {
+    e.preventDefault();
+    var surveyId = $(this).attr('data-id');
+    var $thisButton = $(this);
+
+    $.ajax({
+      url: '/surveys/' + surveyId,
+      type: 'DELETE'
+    }).done(function() {
+      console.log('success deleting survey');
+      $thisButton.parents('.list-group-item').remove();
+    }).fail(function() {
+      console.log("error DELETING SURVEY");
+    });
+
   });
 
+  $('.edit-survey').on('click', function(e) {
+    e.preventDefault();
+    console.log($(this));
+    var questionId = $(this).attr('data-id');
+    $('[data-question="' + questionId + '"]').toggle();
+  });
+
+
 });
-// });
