6462f373a317ead178b5ca5a30639c41e0f4f2b0
maxkrieg <maxwell.krieg@gmail.com>
2015-06-28 11:41:01 -0400

    adds functionality for deleting a question

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 
+  // Hide the edit-question form by default
   $('.edit-question-form').hide();
 
+  // Delete a survey
   $('.delete-survey').on('click', function(e) {
     e.preventDefault();
     var surveyId = $(this).attr('data-id');
@@ -19,11 +21,30 @@ $(document).ready(function() {
 
   });
 
-  $('.edit-survey').on('click', function(e) {
+  // Toggle hide and show of edit-question form
+  $('.edit-question').on('click', function(e) {
     e.preventDefault();
-    console.log($(this));
     var questionId = $(this).attr('data-id');
     $('[data-question="' + questionId + '"]').toggle();
+  });
+
+  // Delete a Question from Survey
+  $('.delete-question').on('click', function(e) {
+    e.preventDefault();
+    var surveyId = $('h2').attr('id');
+    var questionId = $(this).attr('data-id');
+    var $thisButton = $(this);
+
+    $.ajax({
+      url: '/surveys/' + surveyId + '/questions/' + questionId,
+      type: 'DELETE'
+    }).done(function() {
+      console.log('success deleting question');
+      $thisButton.parents('.list-group-item').remove();
+    }).fail(function() {
+      console.log("error DELETING question");
+    });
+
   });
 
 
