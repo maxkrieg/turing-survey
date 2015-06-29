383b0bf8a6735ae1e69170c407181b60caafff0c
Fitfouraking <malinbking@gmail.com>
2015-06-29 16:19:35 -0400

    updates package.json with passport stuff and sets vars in app.js

--- app.js
+++ app.js
@@ -27,11 +27,20 @@ var nib = require('nib');
 //PASSPORT
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 
 // IMPORT SCHEMA
+var User = require('./lib/user.js');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 var Question = require('./lib/question.js');
 var Survey = require('./lib/surveys.js');
-var User = require('./lib/user.js');
+
 
 
 // IMPORT ROUTES
@@ -47,7 +56,23 @@ function compile(str, path) {
   return stylus(str)
     .set('filename', path)
     .use(nib());
-}
+};
+
+//PASSPORT SHIT
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join('./public', 'public')));
+
+
 // set up express to use stylus middlewear and pass in compile function as object
 app.use(stylus.middleware({
   src: __dirname + '/public',
